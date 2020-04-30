6d0e7e9290bb5c9f29f55b002a0071119e3c3358
Nicole Nolla <22096240+sicklesium@users.noreply.github.com>
2020-04-30 00:15:09 -0400

    Endpoints built out

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,37 @@
 const router = require('express').Router();
- 
-router.post('/register',  (req, res) => {
- 
+const db = require('../users/users-model.js');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+router.post('/register', (req, res) => {
+    const user = req.body
+    const hash = bcrypt.hashSync(user.password, 8)
+
+    user.password = hash
+
+    db.add(user)
+        .then(user => {
+            res.status(201).json({ message: "You're registered!" })
+        })
+        .catch(err => {
+            res.status(500).json({ errorMessage: "There was an error registering.", err })
+        })
 });
 
 router.post('/login', (req, res) => {
+    const { username, password } = req.body
 
+    db.findBy({ username })
+        .then(user => {
+            if (user && bcrypt.compareSync(user.password, password)) {
+                res.status(200).json({ message: `You're logged in, ${user.username}!` })
+            } else {
+                res.status(401).json({ errorMessage: "Your credentials were invalid!" })
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ errorMessage: "There was an error logging you in!" })
+        })
 });
 
 module.exports = router;
--- users/users-model.js
+++ users/users-model.js
@@ -4,6 +4,8 @@ const db = require("../data/db-config.js");
 module.exports = {
     getAll,
     findById,
+    findBy,
+    add,
     remove,
     updateUser
 }
@@ -13,22 +15,33 @@ function getAll() {
     return db('users')
 }
 
+function add(user) {
+    return db('users')
+        .insert(user)
+}
+
+function findBy(user) {
+    return db('users')
+        .where(user)
+        .first()
+}
+
 function findById(id) {
     return db('users')
-        .where({id})
+        .where({ id })
         .first()
 }
 
 function remove(id) {
     return db('users')
-    .where({ id })
-    .first()
-    .del();
-  }
+        .where({ id })
+        .first()
+        .del();
+}
 
 
-  function updateUser(id, changes) {
+function updateUser(id, changes) {
     return db('users')
-      .where({id})
-      .update(changes, '*').returning("*");
-  }
+        .where({ id })
+        .update(changes, '*').returning("*");
+}
