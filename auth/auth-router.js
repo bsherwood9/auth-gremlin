985f38c12ebdba36af6be229ac9252dbf6b2bff0
bsherwood9 <36565536+bsherwood9@users.noreply.github.com>
2020-04-30 22:15:52 -0600

    Set up the authentication

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,37 +1,58 @@
-const router = require('express').Router();
-const db = require('../users/users-model.js');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
+const router = require("express").Router();
+const db = require("../users/users-model.js");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-router.post('/register', (req, res) => {
-    const user = req.body
-    const hash = bcrypt.hashSync(user.password, 8)
+router.post("/register", (req, res) => {
+  const user = req.body;
+  const hash = bcrypt.hashSync(user.password, 8);
 
-    user.password = hash
+  user.password = hash;
 
-    db.add(user)
-        .then(user => {
-            res.status(201).json({ message: "You're registered!" })
-        })
-        .catch(err => {
-            res.status(500).json({ errorMessage: "There was an error registering.", err })
-        })
+  db.add(user)
+    .then((user) => {
+      res.status(201).json({ message: "You're registered!" });
+    })
+    .catch((err) => {
+      res
+        .status(500)
+        .json({ errorMessage: "There was an error registering.", err });
+    });
 });
 
-router.post('/login', (req, res) => {
-    const { username, password } = req.body
+router.post("/login", (req, res) => {
+  const { username, password } = req.body;
 
-    db.findBy({ username })
-        .then(user => {
-            if (user && bcrypt.compareSync(user.password, password)) {
-                res.status(200).json({ message: `You're logged in, ${user.username}!` })
-            } else {
-                res.status(401).json({ errorMessage: "Your credentials were invalid!" })
-            }
-        })
-        .catch(err => {
-            res.status(500).json({ errorMessage: "There was an error logging you in!" })
-        })
+  db.findBy({ username })
+    .then((user) => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+        res
+          .status(200)
+          .json({ message: `You're logged in, ${user.username}!`, token });
+      } else {
+        res
+          .status(401)
+          .json({ errorMessage: "Your credentials were invalid!" });
+      }
+    })
+    .catch((err) => {
+      res
+        .status(500)
+        .json({ errorMessage: "There was an error logging you in!" });
+    });
 });
+
+const generateToken = (user) => {
+  const payload = {
+    id: user.id,
+    username: user.username,
+  };
+  const options = {
+    expiresIn: "1d",
+  };
+  const token = jwt.sign(payload, "Awesome Oppossum", options);
+  return token;
+};
 
 module.exports = router;
--- auth/restricted-middleware.js
+++ auth/restricted-middleware.js
@@ -1,3 +1,18 @@
+const jwt = require("jsonwebtoken");
+
 module.exports = (req, res, next) => {
-    res.status(401).json({ heh: 'nice try dude' });
+  const token = req.headers.authorization;
+  if (token) {
+    jwt.verify(token, "Awesome Oppossum", (err, decodedToken) => {
+      if (err) {
+        res.status(401).json({ message: "You done did mess up", err });
+      } else {
+        //sticking the payload on the request
+        req.decodedJwt = decodedToken;
+        next();
+      }
+    });
+  } else {
+    res.status(401).json({ message: "you really messed up, bad. " });
+  }
 };
